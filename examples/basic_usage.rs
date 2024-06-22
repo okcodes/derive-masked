a5ae03ece4ea0f6e9388bd9f1cc60aaa8318a6ef
okcodes <80954089+okcodes@users.noreply.github.com>
2024-06-21 23:43:01 -0600

    Rename

--- examples/basic_usage.rs
+++ examples/basic_usage.rs
@@ -7,15 +7,15 @@ struct User {
 }
 
 fn main() {
-    let u = User {
+    let user = User {
         password: "super_secret".to_string(),
     };
 
     // Should print masked password when printed with different formatters.
 
     // Explicit access to masked field should print the actual value of the field.
-    println!("Password:\n{}", u.password);
-    println!("Display:\n{}", u);
-    println!("Debug:\n{:?}", u);
-    println!("Pretty Debug:\n{:#?}", u);
+    println!("Password:\n{}", user.password);
+    println!("Display:\n{}", user);
+    println!("Debug:\n{:?}", user);
+    println!("Pretty Debug:\n{:#?}", user);
 }
